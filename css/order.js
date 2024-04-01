f98f6132feacc308df900d1d7b15108c869cd114
Ahmed Abo Doh <158000115+ahmedabodooh@users.noreply.github.com>
2024-04-01 19:05:14 +0200

    Add files via upload

---
+++ css/order.js
@@ -0,0 +1,32 @@
+// const Orders=[
+//     {
+//         productName:'foldable mini drone',
+//         productNumber:'85622',
+//         paymentStatus: 'Due',
+//         shipping:'pending'
+//     },
+//     {
+//         productName:'larvender kf102 drone',
+//         productNumber:'52665',
+//         paymentStatus: 'refunded',
+//         shipping:'declined'
+//     },
+//     {
+//         productName:'ruko f11 pro drone',
+//         productNumber:'55633',
+//         paymentStatus: 'Due',
+//         shipping:'pending'
+//     },
+//     {
+//         productName:'gps 4k drone',
+//         productNumber:'88232',
+//         paymentStatus: 'paid',
+//         shipping:'deliverd'
+//     },
+//     {
+//         productName:'drone with camera drone',
+//         productNumber:'29889',
+//         paymentStatus: 'paid',
+//         shipping:'deliverd'
+//     }
+// ]
